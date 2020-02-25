14c322cf389d0478e1cf0a710f5703aef7206dd2
Prerak143-stack <61062101+Prerak143-stack@users.noreply.github.com>
2020-02-24 21:39:10 -0500

    Add files via upload

---
+++ answerThePhone.py
@@ -0,0 +1,33 @@
+# I am Prerak Patel and my student number 000736376 I myself certify that all
+# the code is written by myself  and submitted I also clarify that I have not
+# allowed my work to be copied by others
+
+print("Hello sir, you got a call\n")
+
+# GET the input from the user is he sleeping or not
+usr_ans = input("Are you sleeping or not ?\n")
+
+# IF 'yes' then end the program
+if usr_ans == "yes":
+    print("Sorry to disturb sir\n")
+
+# IF 'no' then take the input from the user to is it a mom's call
+else :
+    mom_call = input("Is it mom's call\n")
+
+# IF 'yes' answer it and end the program
+    if mom_call == "yes":
+        print("Answer it\n")
+
+# IF 'no' then take input from the user to is it morning        
+    else :
+        morning = input("Is it morning ?\n")
+
+# IF 'yes' then end the program
+        if morning == "yes":
+            print("don't answer\n")
+
+# IF 'no' then 'answer it' and end the program
+        else :
+            print("answer it\n")
+            
